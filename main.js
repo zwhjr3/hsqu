78e62fc715421ad86a23a375b2c952875db18cfd
zwhjr3 <82552046+zwhjr3@users.noreply.github.com>
2023-06-14 14:50:15 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,66 @@
+// Create a reference for the canvas
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
+
+img_width = 300;
+img_height = 100;
+
+var img_image;
+
+img_x = 100;
+img_y = 100;
+
+function add() {
+	img_imgTag = new Image(); //defining a variable with a new image
+	img_imgTag.onload = uploadimg; // setting a function, onloading this variable
+	img_imgTag.src = img_image;   // load image
+}
+
+function uploadimg() {
+
+	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
+}
+
+//Write a code to grab the key-pressed event
+window.addEventListener("keydown" , my_keydown);
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	
+		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
+		alphabetkey();
+		document.getElementById("d1").innerHTML="You pressed Alphabet Key";
+		console.log("alphabet key")
+	else 
+	
+	{
+
+		otherkey();
+		document.getElementById("d1").innerHTML="You pressed symbol or other key";
+	}
+}
+
+function alphabetkey()
+{
+	//upload respective image with the message. 
+img_image="Alpkey.png";
+add();
+}
+function numberkey()
+{
+	
+}
+function arrowkey()
+{
+}
+function specialkey()
+{
+	
+}
+function otherkey()
+{
+	img_image="otherkey.png";
+	add();
+}
+	
